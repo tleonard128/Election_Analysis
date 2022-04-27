8c7eb3c880cb258388caf4b87935317fffac462e
Tom Leonard <tleonard@plaid.com>
2022-04-27 09:57:57 -0400

    updating election analysis with all practice data

---
+++ Python_practice.py
@@ -0,0 +1,50 @@
+# print("Hello World")
+# counties = ["Arapahoe","Denver", "Jefferson"]
+
+# # if "El Paso" in counties or "Arapahoe" in counties:
+
+# #     print("El Paso or Arapahoe is in the list of counties. ")
+
+# # else:
+# #     print("El Paso and Arapahoe are not in the list of counties. ")
+
+# for county in counties:
+   
+#     print(county)
+
+# from multiprocessing.sharedctypes import Value
+
+
+
+
+# counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+
+
+# for county, voters in counties_dict.items():
+
+#     print(f'{county} county has {voters} registered voters.')
+
+# voting_data = [{"county":"Arapahoe", "registered_voters": 422829},
+#                 {"county":"Denver", "registered_voters": 463353},
+#                 {"county":"Jefferson", "registered_voters": 432438}]
+
+# for county_dict in voting_data:
+#        print(f"{county_dict['county']} county has {county_dict['registered_voters']:,} registered voters.")
+
+# for county_dict in voting_data:
+
+#         print(county_dict['county'])
+
+# candidate_votes = int(input("How many votes did the candidate get in the election?"))
+
+# total_votes = int(input('What is the total number of votes in the election?'))
+
+# message_to_candidate = (
+
+#         f'You received {candidate_votes:,} number of votes.'
+        
+#         f'The total numer of votes in the election was {total_votes:,}.'
+
+#         f'You revieved {candidate_votes/total_votes * 100:.2f}% of the total votes.')
+
+# print(message_to_candidate)
