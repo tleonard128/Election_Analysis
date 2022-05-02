51d113fa78ffb126fd0a5005315a625194010956
Tom Leonard <tleonard@plaid.com>
2022-05-01 23:01:18 -0400

    adding updates in Election Analysis

--- PyPoll.py
+++ PyPoll.py
@@ -1,4 +1,33 @@
 # The data we need to retrieve.
+import csv
+import os
+
+# Assign a variable for the file to load and the path
+file_to_load = os.path.join("Resources", "election_results.csv")
+
+#Create a filename variable to a direct or indirect path to the file
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+
+#Open the election results and read the file.
+with open(file_to_load) as election_data:
+
+
+#To do: read and analyze the data here.
+
+    #Read the file object with the reader function.
+    file_reader = csv.reader(election_data)
+    
+    #Print the header row.
+    headers = next(file_reader)
+    
+    print(headers)
+
+
+
+
+
+
 # 1. The total number of votes cast
 # 2. A complete list of candidates who received votes
 # 3. The percentage of votes each candidate won
