cfa741b1762df7fd8a2d66c92dc3fc8de3514387
Tom Leonard <tleonard@plaid.com>
2022-05-04 22:18:28 -0400

    Adding the election audit code.

--- PyPoll.py
+++ PyPoll.py
@@ -16,6 +16,12 @@ candidate_options = []
 #Create Dictionary for votes
 candidate_votes = {}
 
+#Winning Candidate and Winning Count Tracker
+winning_candidate = ""
+
+winning_count = 0
+
+winning_percentage = 0
 
 #Open the election results and read the file.
 with open(file_to_load) as election_data:
@@ -57,7 +63,35 @@ for candidate_name in candidate_votes:
     #Calculate the percentage of votes.
     vote_percentage = float(votes) / float(total_votes) * 100
 
-    print(f'{candidate_name}: recieved {vote_percentage:.1f}% of the vote.')
+   #Print out each candidate's name, vote count and percentage of votes
+    print(f"{candidate_name}: {vote_percentage: .1f}% ({votes:,})")
+
+    # 1. Determine if the votes are greater than the winning count.
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+
+        # 2. If true then set winning_count = votes and winning_percentage = 
+        #vote_percentage.
+        winning_count = votes
+
+        winning_percentage = vote_percentage
+
+        winning_candidate = candidate_name
+
+# To do: print out the winning candidate, vote count, and percentage
+winning_candidate_summary = (
+    f"------------------------\n"
+
+    f"Winner: {winning_candidate}\n"
+
+    f"Winning Vote Count: {winning_count:,}\n"
+
+    f"Winning Percentage: {winning_percentage: .1f}%\n"
+
+    f"--------------------------\n")
+
+print(winning_candidate_summary)
+
+
 
 
 
